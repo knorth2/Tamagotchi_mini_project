b9e507bf13ee4ee1082d0a00a94ae0a3a8e519a9
knorth <kayci.north@gmail.com>
2022-07-06 15:05:02 -0600

    skeleton code

---
+++ js/app.js
@@ -0,0 +1,14 @@
+class Character{
+    constructor(name, age, hunger, sleepiness, boredom){
+        this.name = name;
+        this.age = age;
+        this.hunger = hunger;
+        this.sleepiness = sleepiness;
+        this.boredom = boredom
+    }
+}
+//Intro to the Game.
+// namePet(promptName){
+//     this.name = prompt('Welcome to Bugs Life! To keep your bug alive, click the buttons to feed, play or rest your little friend. The buttons will go up one point for every minute! If any of the buttons reaches 10 your bug will die. So the more you interact with your bug the greater the chance you will keep it alive! Some features of this game include your bug morphing when the timer reaches 0. To proceed please type a name for your bug below and click ok.')
+// }
+// const Character = new Character ('Ziggy Bug')

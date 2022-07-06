6959239929674bcba0509278f5b17829d7e57363
knorth <kayci.north@gmail.com>
2022-07-06 17:30:45 -0600

    pseudoCode

--- js/app.js
+++ js/app.js
@@ -1,14 +1,39 @@
-class Character{
-    constructor(name, age, hunger, sleepiness, boredom){
-        this.name = name;
-        this.age = age;
-        this.hunger = hunger;
-        this.sleepiness = sleepiness;
-        this.boredom = boredom
-    }
-}
+//pseudoCode
+//Make an alert and prompt to add name of bug and to give rules of game. 
+
+//Event Listeners-Make start button that starts the game/timer?
+
+//Create buttons to keep bug alive, hunger, play, sleep(lights on/off) with a 1-10 scale.  There will also be metrics keeping track of hunger, sleepiness, play/boredom, and age. 
+
+//Create object with functions/methods with if/else if statements.
+
+//Morphing will require bug to stay alive for how long? 120secs? Bug will die if hunger, play, and sleepiness hits 10.
+
+//Create morphed bug? 2 or 3 stages with setAge function inside game object?
+
+//What will you use to keep track of 120sec of life. A callback function/higher order function?
+
+//Game ends when bug dies. Alert bug has died.
+
+
+
+
+// class Character{
+//     constructor(name, age, hunger, sleepiness, boredom){
+//         this.name = name;
+//         this.age = age;
+//         this.hunger = hunger;
+//         this.sleepiness = sleepiness;
+//         this.boredom = boredom
+//     }
+// }
 //Intro to the Game.
 // namePet(promptName){
 //     this.name = prompt('Welcome to Bugs Life! To keep your bug alive, click the buttons to feed, play or rest your little friend. The buttons will go up one point for every minute! If any of the buttons reaches 10 your bug will die. So the more you interact with your bug the greater the chance you will keep it alive! Some features of this game include your bug morphing when the timer reaches 0. To proceed please type a name for your bug below and click ok.')
 // }
 // const Character = new Character ('Ziggy Bug')
+
+
+
+
+

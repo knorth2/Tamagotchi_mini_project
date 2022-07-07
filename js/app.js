aa4ca2f8a7fe7f016e205b547ac728b97f1b4f4f
knorth <kayci.north@gmail.com>
2022-07-06 18:02:35 -0600

    Created Parent class and methods

--- js/app.js
+++ js/app.js
@@ -11,24 +11,48 @@
 
 //Create morphed bug? 2 or 3 stages with setAge function inside game object?
 
-//What will you use to keep track of 120sec of life. A callback function/higher order function?
+//What will you use to keep track of 120sec of life. setInterval() method calls a function at specified intervals(in milliseconds). The setInterval()method continues calling the function until the clearInterval() is called, or the window is closed. 1sec = 1000milliseconds.
 
 //Game ends when bug dies. Alert bug has died.
 
 
+//Global variables
+const startTime = 120;
+let time = startTime;
+let age = 1;
+
+//Parent Class - instantiate from parent class and make methods
+
+class Character{
+    constructor(name, hunger, sleepiness, boredom){
+        this.name = name;
+        this.hunger = hunger;
+        this.sleepiness = sleepiness;
+        this.boredom = boredom
+    }
+}
+
+class Bug extends Character{
+    constructor(name, hunger, sleepiness, boredom){
+        super(name, hunger, sleepiness, boredom)
+        //might put animations here? this.animations
+    }
+    feed(){
+        this.hunger++
+    }
+    sleep(){
+        this.sleepiness++
+    }
+    play(){
+        this.boredom++
+    }
+}
+const Bug = new Bug
+  
 
 
-// class Character{
-//     constructor(name, age, hunger, sleepiness, boredom){
-//         this.name = name;
-//         this.age = age;
-//         this.hunger = hunger;
-//         this.sleepiness = sleepiness;
-//         this.boredom = boredom
-//     }
-// }
 //Intro to the Game.
-// namePet(promptName){
+// nameBug(promptName){
 //     this.name = prompt('Welcome to Bugs Life! To keep your bug alive, click the buttons to feed, play or rest your little friend. The buttons will go up one point for every minute! If any of the buttons reaches 10 your bug will die. So the more you interact with your bug the greater the chance you will keep it alive! Some features of this game include your bug morphing when the timer reaches 0. To proceed please type a name for your bug below and click ok.')
 // }
 // const Character = new Character ('Ziggy Bug')
